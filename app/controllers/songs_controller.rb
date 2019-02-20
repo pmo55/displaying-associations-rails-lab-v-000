ab105fe7468c5f3a8226a5f3010f516de5db5b63
Parsa Moshiri <parsamoshiri5@gmail.com>
2019-02-20 16:31:25 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,11 +1,15 @@
 class SongsController < ApplicationController
   def index
+    @songs = Song.all
   end
 
   def show
+    @song = Song.find(params[:id])
   end
 
   def new
+    @song = Song.new
+    @artists = Artist.all
   end
 
   def create
